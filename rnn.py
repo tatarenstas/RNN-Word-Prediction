0bb66ec6fa2bdb8b25a4c4f771eb9f0d177553eb
Stanislav Tataren <79329397+tatarenstas@users.noreply.github.com>
2021-12-30 15:57:50 +0200

    Update rnn.py

--- rnn.py
+++ rnn.py
@@ -1,46 +1,49 @@
-import numpy as np
-from tensorflow.keras.layers import Dense, SimpleRNN, Input, Embedding
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.preprocessing.text import Tokenizer, text_to_word_sequence
-from tensorflow.keras.utils import to_categorical
+import numpy as np
 
-with open('text', 'r', encoding='utf-8') as f:
-    texts = f.read()
-    texts = texts.replace('\ufeff', '')
+from tensorflow.keras.layers import Dense, SimpleRNN, Input
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing.text import Tokenizer, text_to_word_sequence
+from tensorflow.keras.utils import to_categorical
 
-maxWordsCount = 1000
-tokenizer = Tokenizer(num_words=maxWordsCount, filters='!–"—#$%&amp;()*+,-./:;<=>?@[\\]^_`{|}~\t\n\r«»',
-                      lower=True, split=' ', char_level=False)
+with open('text', 'r', encoding='utf-8') as f:
+    texts = f.read()
+    texts = texts.replace('\ufeff', '')
+
+maxWordsCount = 1000
+tokenizer = Tokenizer(num_words=maxWordsCount, filters='!–"—#$%&amp;()*+,-./:;<=>?@[\\]^_`{|}~\t\n\r«»',
+                      lower=True, split=' ', char_level=False)
 tokenizer.fit_on_texts([texts])
 
-dist = list(tokenizer.word_counts.items())
+dist = list(tokenizer.word_counts.items())
 print(dist[:10])
 
-data = tokenizer.texts_to_sequences([texts])
-res = np.array( data[0] )
+data = tokenizer.texts_to_sequences([texts])
+res = to_categorical(data[0], num_classes=maxWordsCount)
+print(res.shape)
 
-inp_words = 3
-n = res.shape[0] - inp_words
+inp_words = 3
+n = res.shape[0] - inp_words
 
-X = np.array([res[i:i + inp_words] for i in range(n)])
-Y = to_categorical(res[inp_words:], num_classes=maxWordsCount)
+X = np.array([res[i:i + inp_words, :] for i in range(n)])
+Y = res[inp_words:]
 
-model = Sequential()
-model.add(Embedding(maxWordsCount, 256, input_length = inp_words))
-model.add(SimpleRNN(128, activation='tanh'))
-model.add(Dense(maxWordsCount, activation='softmax'))
+model = Sequential()
+model.add(Input((inp_words, maxWordsCount)))
+model.add(SimpleRNN(128, activation='tanh'))
+model.add(Dense(maxWordsCount, activation='softmax'))
 model.summary()
 
-model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
+model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
 
-history = model.fit(X, Y, batch_size=32, epochs=50)
+history = model.fit(X, Y, batch_size=32, epochs=50)
+
 
 def buildPhrase(texts, str_len=20):
     res = texts
     data = tokenizer.texts_to_sequences([texts])[0]
     for i in range(str_len):
-        x = data[i: i + inp_words]
-        inp = np.expand_dims(x, axis=0)
+        x = to_categorical(data[i: i + inp_words], num_classes=maxWordsCount)
+        inp = x.reshape(1, inp_words, maxWordsCount)
 
         pred = model.predict(inp)
         indx = pred.argmax(axis=1)[0]
@@ -49,6 +52,7 @@ def buildPhrase(texts, str_len=20):
         res += " " + tokenizer.index_word[indx]
 
     return res
+
 
 res = buildPhrase("preference partiality")
 print(res) #preference partiality not ...
